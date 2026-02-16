0ee014831cd33c893dd5ecdc15d355b78c0928b9
richard.amaral <richard.amaral.jf@gmail.com>
2026-02-15 22:33:32 -0300

    git commit -m "style(register): aplicar identidade visual e ajustes de interface na página de registro"

--- src/app/auth/Register/registerView.tsx
+++ src/app/auth/Register/registerView.tsx
@@ -4,6 +4,21 @@ import { LinearGradient } from 'expo-linear-gradient';
 import MaskedView from '@react-native-masked-view/masked-view';
 import { styles } from "./styles";
 import { Colors } from "@/theme/colors";
+import { InputFormView } from "@/components/InputForm/InputFormView"
+
+export const GradientText = (props) => {
+    return (
+    <MaskedView maskElement={<Text {...props} />}>
+        <LinearGradient
+        colors={[Colors.white_1, Colors.purple_4, Colors.purple_2]} // Do roxo para o azul
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
+        >
+        <Text {...props} style={[props.style, { opacity: 0 }]} />
+        </LinearGradient>
+    </MaskedView>
+    );
+};
 
 export function RegisterView() {
     return (
@@ -14,10 +29,27 @@ export function RegisterView() {
         <View style={styles.overlay}>    
             <StatusBar barStyle="light-content" translucent backgroundColor="transparent"/>
             <View style={styles.container}>
-
-            <Text style={styles.textPlaceholder}>Página padrão</Text>
-
-
+                <View style={styles.topPage}></View>
+                <View style={styles.logoConteiner}>
+                <Image
+                source={require("@/assets/images/siteImages/KomiImpactLogo.png")}
+                style={styles.logoImage}/>
+                <GradientText style={styles.title}>KOMI IMPACT</GradientText>
+                </View>
+                <View style={styles.formsContainer}>
+                    <Text style={styles.loginTitle}>REGISTRE-SE</Text>
+                    <Text style={styles.loginSubtitle}>Digite duas credenciais para continuar</Text>
+                    <InputFormView formPlaceholder="Digite seu nome..."></InputFormView>
+                    <InputFormView formPlaceholder="Digite seu email..."></InputFormView>
+                    <InputFormView formPlaceholder="Digite sua senha..."></InputFormView>
+                    <View style={styles.registerForm}>
+                        <Text style={styles.rigisterTitle}>Já possui reigistro?</Text>
+                        <Text style={styles.registerSubtitle}>Login</Text>
+                    </View>
+                    <TouchableOpacity style={styles.buttonLogin}>
+                        <Text style={styles.buttonText}>LOGIN</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
         </ImageBackground>
--- src/app/auth/Register/styles.tsx
+++ src/app/auth/Register/styles.tsx
@@ -5,25 +5,89 @@ import { width, height } from "@/constants/Dimenions";
 export const styles = StyleSheet.create({
     background: {
     flex: 1,
-    width: "100%",
-    height: "100%",
+        width: "100%",
+        height: "100%",
     },
     overlay: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgba(46, 37, 66, 0.80)",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgba(46, 37, 66, 0.80)",
     },
     container: {
     flex: 1,
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
     },
-    textPlaceholder: {
-    fontFamily: "FontTitleBold",
-    fontSize: 40,
-    color: Colors.white_1,
+    topPage: {
+        width: "100%",
+        height: height * 0.1,
     },
+    logoConteiner: {
+        width: "100%",
+        height: height * 0.3,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    logoImage: {
+        width: width * 0.9,
+        height: height * 0.35,
+        resizeMode: "contain",
+    },
+    title: {
+        fontFamily: "FontTitleBold",
+        fontSize: 32,
+    },
+    formsContainer: {
+        width: "100%",
+        height: height * 0.6,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+    },
+    loginTitle: {
+        fontFamily: "FontTitleBold",
+        fontSize: 40,
+        color: Colors.white_1,
+    },
+    loginSubtitle: {
+        fontFamily: "FontTitleBold",
+        fontSize: 12,
+        color: Colors.white_1,
+    },
+    registerForm: {
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        gap: 5,
+        paddingHorizontal: width * 0.11
+    },
+    rigisterTitle: {
+        fontFamily: "FontTitleBold",
+        fontSize: 10,
+        color: Colors.white_1,
+    },
+    registerSubtitle: {
+        fontFamily: "FontTitleBold",
+        fontSize: 10,
+        color: Colors.purple_2,
+    },
+    buttonLogin: {
+        backgroundColor: "rgba(123, 92, 255, 0.8)",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 16,
+        width: width * 0.8,
+        paddingVertical: 10,
+        borderWidth: 1,
+        borderColor: 'rgba(242, 241, 246, 0.2)',
+    },
+    buttonText: {
+        fontFamily: "FontTitleBold",
+        fontSize: 15,
+        color: Colors.white_1,
+    }
 });
